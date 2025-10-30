e1a3edba1dd456a56018c38fda8a9b727b75e823
harshit25csu080-ncu <harshit25csu080@ncuindia.edu>
2025-10-31 00:28:14 +0530

    Update Q10.c

--- Q10.c
+++ Q10.c
@@ -8,15 +8,18 @@ int main(){
     int marks[n];
 
     for(int i=0;i<n;i++){
-        printf("Enter marks: ");
+        printf("Enter marks of %d student : ",i+1);
         scanf("%d",&marks[i]);
     }
 
     for(int j=0;j<n;j++){
         if(marks[j]==99){
             count++;
+            printf("%dth student got 99 marks\n",j+1);
         }
     }
     printf("The Number of students who scored 99 marks are %d",count);
-    return 0;
+    return 0 ;
+
 }
+
